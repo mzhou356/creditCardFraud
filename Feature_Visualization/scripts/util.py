72c27eb95e89a0340f46ef3049d7b88e4a81e182
Mindy <minzhou16@gmail.com>
2020-07-24 18:35:03 -0400

    Add plot_umap for embed transform

--- Feature_Visualization/scripts/util.py
+++ Feature_Visualization/scripts/util.py
@@ -45,27 +45,24 @@ def create_umapDF(data, label, sample_size,seed):
     X, y = feature_df.drop(label,axis=1), feature_df[label]
     return X,y
     
-def plot_umap(embed,title,target):
+def plot_umap(embed,label,title):
     """
     This function plots umap 2D embed.
     
+    Arg:
     embed: umap embed, 2D 
-    title: title for the graph 
-    target: the classification label 
-    
+    label: a pandas series, label for the fraud and normal
+    title: a string, indicates the title of the plot
+
     plots a scatter plot of component 1 and component 2
     """
-    plt.figure(figsize=(12,7),dpi=150)
-    cdict = {0:"yellow",1:"purple"}
-    n_ind = np.where(target==0)
-    f_ind = np.where(target==1)
-    plt.scatter(embed[n_ind,0],embed[n_ind,1],s=1,c=cdict[0],label="normal",cmap="viridis")
-    plt.scatter(embed[f_ind,0],embed[f_ind,1],s=3,c=cdict[1],label="fraud",cmap="viridis")
+    plt.figure(figsize=(10,7), dpi=120)
+    plt.scatter(embed[label== 0,0], embed[label== 0,1], s=1, c='C0', cmap='fire', label="normal")
+    plt.scatter(embed[label== 1,0], embed[label== 1,1], s=3, c='C1', cmap='fire', label="fraud")
     plt.title(title)
-    plt.legend(fontsize=12,markerscale=3)
-    plt.xlabel("$x_0$")
-    plt.ylabel("$x_1$")
+    plt.xlabel("component 1")
+    plt.ylabel("component 2")
+    plt.legend(fontsize=16, markerscale=5)
     plt.show()
-    
 
  
